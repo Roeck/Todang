5e96be4237dcf6921d0749460592bcd8f3706eb4
Roeck <rons9n@gmail.com>
2021-02-18 17:49:43 -0500

    submitting new todos functionality

--- src/app/components/todos/todos.component.ts
+++ src/app/components/todos/todos.component.ts
@@ -8,6 +8,7 @@ import { Todo } from './../../models/Todo';
 })
 export class TodosComponent implements OnInit {
   todos: Todo[];
+  inputTodo: string = '';
   constructor() {}
 
   ngOnInit(): void {
@@ -33,5 +34,13 @@ export class TodosComponent implements OnInit {
 
   deleteTodo(id: number) {
     this.todos = this.todos.filter((v, i) => i !== id);
+  }
+
+  addTodo() {
+    this.todos.push({
+      content: this.inputTodo,
+      completed: false
+    });
+    this.inputTodo = '';
   }
 }
